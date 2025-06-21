7be1979bca4da9ecde4d5b1654cc2164b9bfe39a
Abel <105869103+nanonexpy@users.noreply.github.com>
2025-06-21 19:30:37 -0300

    anecdotes final

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  function changeAnecdote(min, max) {
+    const minimum= Math.ceil(min);
+    const maximum = Math.floor(max);
+    return Math.floor(Math.random() * (maximum - minimum + 1) + minimum);
+  }
+
+  const handleVote = () => {
+    setVotes((previousVotes) => {
+      const newVotes = [...previousVotes];
+      newVotes[selected] += 1;
+      return newVotes;
+    })
+  }
+
+  return (
+    <div>
+      {anecdotes[selected]} <br></br> has {votes[selected]} votes.
+      <br></br>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={() => setSelected(changeAnecdote(0, anecdotes.length - 1))}>
+        next anecdote
+      </button>
+
+      <h1>Anecdotes with most votes</h1>
+      <div>{anecdotes[votes.indexOf(Math.max(...votes))]}</div>
+    </div>
+  )
+}
+
+export default App
